ac13ab0db21a72b19caf8f9103ab531e33c7490a
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-11-08 23:25:19 +0630

    feat : pratice array.

--- index.php
+++ index.php
@@ -1,39 +1,21 @@
-
-
 <?php
-//String
-$first_name = 'Mg Mg';
-$last_name = "Lwin";
 
-echo  'Your name is ' . $first_name . ' ' . $last_name;
+$users = [
+    ["name" => "Aung", "age" => 18],
+    ["name" => "Kyaw", "age" => 16],
+    ["name" => "Min", "age" => 20],
+    ["name" => "Lwin", "age" => 25],
+];
+echo $users[0]['name'];
 
-//number 
-$age = 18;
-$limit = 18;
 
-$days = 18 * 365;
-echo "<div> You have passed $days days </div>";
 
-for ($i = 0; $i < 10; $i++) {
-    echo "<ul><li>$i</li></ul>";
+for ($i = 0; $i < count($users); $i++) {
+    echo  $users[$i]["name"] . "\n";
 }
 
-$v = 0;
-while ($v < 10) {
-    echo $v . " days";
-    $v++;
+foreach ($users as $index => $user) {
+    echo "<li> {$user["name"]}</li>";
 }
 
-//arrays
-
-$usernames = ["abc", "bbc", "ccc"];
-foreach ($usernames as $name) {
-    echo  "<h1> $name </h1>";
-}
-
-function sum_number($a, $b)
-{
-    return $a + $b;
-}
-
-echo sum_number(100, 202);
+array_push($usrs, ['name' => "Ko", "age" => 12]);
