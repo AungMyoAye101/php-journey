f4d280d7e6f59655e9bdcc9ffd51d5d77aeeaaf4
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-11-13 21:29:47 +0630

    string mrthod.

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
 <?php
 
-
-$users = [["name" => "Aung", "age" => 20], ["name" => "Maung", "age" => 20]];
-
-
-for ($i = 0; $i < count($users); $i++) {
-    echo " {$users[$i]['name']} age {$users[$i]['age']}<br>";
-}
-
-foreach ($users as $key => $value) {
-    echo "$key : name = {$value['name']} , value = {$value['age']} <br>";
-}
-
-$colors = ["code" => 'red', "code" => 'green', "code" => 'blue', "code" => 'orange'];
-foreach ($colors as $key => $v) {
-    echo $colors[$key];
-}
-$array = [
-    ["a", 'b', 'v'],
-    [3, 4, 6],
-];
-
-foreach ($array as [$a, $b]) {
-    // Note that there is no $c here.
-    echo "$a $b <br>";
-}
+$name = "Aung";
+echo "$name <br>";
+echo strlen($name) . " String lenght <br>";
+echo strpos($name, "g") . '<br>';
+echo substr($name, 1, strlen($name)) . '<br>';
+echo str_replace("A", "M", $name) . "<br>";
+echo trim("   hello ");
+echo implode('_', ["a", "b", "c"]);
+echo explode('', $name);
