5d4ea953746f6e300fd5dcd32a0c21ff9b74ec0a
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-11-14 22:32:13 +0630

    paratic simple login,logout and upload file

--- index.php
+++ index.php
@@ -14,3 +14,18 @@ echo $name;
 <h1>Welcome <?= $name ?></h1>
 
 <a href="profile.php"> profile</a>
+
+<form action="index.php" method="POST">
+    <button type="submit" style="background:red;padding:4px 1rem ; color:wheat">Logout</button>
+</form>
+
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    session_destroy();
+    setcookie("name", "", time() - 3600);
+    header("Location: login.php");
+    exit();
+}
+?>
--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 session_start();
 $name = $_SESSION["name"];
 ?>
-<h1 style="color:darkblue">Your name is : $name</h1>
+<h1 style="color:darkblue">Your name is : <?= $name ?></h1>
 <form action="profile.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="photo">
     <button type="submit">Upload</button>
@@ -11,5 +11,12 @@ $name = $_SESSION["name"];
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $file = $_FILES['photo'];
+    if ($file['error'] === 0) {
+        move_uploaded_file($file['tmp_name'], "uploads/" . $file['name']);
+    };
     echo $file['name'];
+    echo "<img src='uploads/{$file['name']}' alt='{$file['name']}' style='width:120px'>";
 }
+
+?>
+<a href="index.php"> Back to home </a>
